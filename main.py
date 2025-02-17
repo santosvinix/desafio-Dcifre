4ab043b6cb884b6062e5688708fd10b3ea5ca4f0
Vinícius <vinicius.santos.engineer@gmail.com>
2025-02-17 00:49:37 -0300

    resolvido o problema das variáveis de ambiente

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
 from sqlalchemy import create_engine, Column, String, Integer, ForeignKey 
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
+# Carregar o .env
+load_dotenv()
+
+print(os.getenv('DB_USUARIO'), os.getenv('DB_SENHA'), os.getenv('DB_HOST'), os.getenv('DB_NOME'))
+
+# Criando o banco de dados 'empresas' em MySQL
+db = create_engine(f"mysql+pymysql://{os.getenv('DB_USUARIO')}:{os.getenv('DB_SENHA')}@{os.getenv('DB_HOST')}:3306/{os.getenv('DB_NOME')}.db")
+Session = sessionmaker(bind=db)
+session = Session()
+
+# Criando as tabelas
+Base = declarative_base()
+Base.metadata.create_all(bind=db)
+
+
+
+# Utilizando o FastAPI
 app = FastAPI()
 
 
